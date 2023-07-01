f6b164314f3ddc8557d720fb53294d4c9aa48818
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-07-01 17:07:19 +0000

    Added props & conditions to Card component

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import PropType from 'prop-types'
 
-const Card = () => {
+const Card = (props) => {
+  const imgStyle = {
+    objectFit: "cover",
+    width: "100%",
+    aspectRatio: "3/2"
+  }
   return (
-    <div className="card">
-        <img src="https://placehold.co/600x400" className="card-img-top" alt="..." />
+    <div className="card mt-2" >
+        <img src={props.imgURL ? props.imgURL : "https://placehold.co/600x400"} className="card-img-top" alt="..." style={imgStyle}/>
         <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-title">{props.cardTitle ? props.cardTitle : 'Card Title'}</h5>
+            <p className="card-text">{props.cardText ? props.cardText : 'There is no text. Please put some in, you little goober'}</p>
             <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
     </div>
   )
 }
-
+Card.PropType = {
+  imgURL: PropType.string,
+  cardTitle: PropType.string,
+  cardText: PropType.string
+}
 export default Card
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,16 +11,16 @@ const Home = () => {
 				<Jumbotron />
 				<div className="row">
 					<div className="col">
-						<Card />
+						<Card imgURL="https://i.pinimg.com/originals/d1/73/59/d173596bfc14577a6a6e923b2c25d71a.gif" />
 					</div>
 					<div className="col">
 						<Card />
 					</div>
 					<div className="col">
-						<Card />
+						<Card cardText="This card does have some text. I guess you're not a goober" />
 					</div>
 					<div className="col">
-						<Card />
+						<Card cardTitle="Custom Title" />
 					</div>
 				</div>
 			</div>
